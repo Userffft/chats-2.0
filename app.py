2e3ab67a3bb992305bc95c04fa4358a4ec3a64d6
Userffft <134155306+Userffft@users.noreply.github.com>
2026-04-28 21:04:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -465,4 +465,4 @@ def handle_join(data):
     join_room(room)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
